dd9ae639e438ce8e0b7d694598d3762bd4088bf4
kskarthik <kskarthik91@gmail.com>
2017-10-03 17:25:05 +0530

    another try

--- inject.js
+++ inject.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
      script.type = 'text/javascript';  
-     script.src = "https://hide.js"
+     script.src = "https://raw.githubusercontent.com/kskarthik/ProSig/master/hide.js"
 document.body.appendChild(script);
  
 //function hidesig() { sig = document.getElementsByClassName('protonmail_signature_block-proton'); sig.parentNode.removeChild(sig); } document.getElementsByClassName('compose').onlick = function() { hidesig(); }
