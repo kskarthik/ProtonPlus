c8aac2fd03566a18a46efb7d67fde6abec0a67d5
kskarthik <kskarthik91@gmail.com>
2017-10-04 19:59:16 +0530

    another attempt

--- hide.js
+++
@@ -1,9 +0,0 @@
-function hidesig() { 
-
-      sig = document.getElementsByClassName('protonmail_signature_block-proton'); 
-      sig.parentNode.removeChild(sig); 
-} 
-
-     document.getElementsByClassName('compose').onlick = function() { hidesig(); };
-
-alert ('hide js loaded!')
--- inject.js
+++ inject.js
@@ -1,7 +1,9 @@
 var script = document.createElement('script');
      script.type = 'text/javascript';  
-     script.src = "https://raw.githubusercontent.com/kskarthik/ProSig/master/hide.js"
-document.body.appendChild(script);
+     script.textContent = `
+     document.getelementsByClassName('compose').addEventListener("click", function() {document.getElementsByClassName('protonmail_signature_block-proton').setAttribute = ('style', 'display: none');} ); `;   
+ 
+     document.head.appendChild(script); 
  
 //function hidesig() { sig = document.getElementsByClassName('protonmail_signature_block-proton'); sig.parentNode.removeChild(sig); } document.getElementsByClassName('compose').onlick = function() { hidesig(); }
 
