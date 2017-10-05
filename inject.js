4be38776f7906e7742868695b5aedecf7c91bb5e
kskarthik <kskarthik91@gmail.com>
2017-10-05 21:55:09 +0530

    another try :(

--- inject.js
+++ inject.js
@@ -1,9 +1,26 @@
-var script = document.createElement('script');
+let script = document.createElement('script');
      script.type = 'text/javascript';  
      script.textContent = `
-     document.getelementsByClassName('compose').addEventListener("click", function() {document.getElementsByClassName('protonmail_signature_block-proton').setAttribute = ('style', 'display: none');} ); `;   
- 
-     document.head.appendChild(script); 
+      function Start() { 
+         
+         document.body.getElementsByClassName("compose").onclick = (function () { Hide(); });
+         
+         }; 
+          
+          Start();
+     
+     function Hide() { 
+
+             let id = document.getElementById("pm_composer");
+             let cl = getElementsByClassName("protonmail_signature_block-proton");
+                id.cl.innerHTML=" "; // id.cl.setAttribute = ("style", "display: none");  
+              };
+  
+        
+          
+         `;
+      
+       document.head.appendChild(script); 
  
 //function hidesig() { sig = document.getElementsByClassName('protonmail_signature_block-proton'); sig.parentNode.removeChild(sig); } document.getElementsByClassName('compose').onlick = function() { hidesig(); }
 
