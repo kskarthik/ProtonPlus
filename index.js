3473d7da3fbe77e0265948561fa57471a98c6745
kskarthik <kskarthik91@gmail.com>
2017-11-05 14:14:20 +0530

    update code

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-/* 
-Name: Proton+ | Author: Sai Karthik | Licence: MPL v2 
-*/
+/* Name: Proton+ | Author: Sai Karthik <kskarthik@protonmail.com> | Licence: MPL v2  */
 
 'use strict';
 
+// change color of compose button
 function colorCompose(){
 	document.getElementsByClassName("compose")[0].style.backgroundColor = "#d11919";
-    Replace();
+    
+          Replace();
 }
+setInterval(colorCompose, 5000);
 
- colorCompose();
- 
+// replace glyphicon with appropriate one 
 function Replace() {
 	let x = document.getElementsByClassName("fa-life-ring")[0];
 
          x.classList.add("fa-bug");	 
          x.classList.remove("fa-life-ring");
+           
+           mobileView();
 }
+function mobileView() {
+
+    document.getElementsByClassName("headerSecuredMobile-compose")[0].getElementsByTagName('div')[0].style.backgroundColor = "#d11919";
+
+         document.getElementsByTagName("footer")[0].style.position = "fixed";
+}
+/*disable protonmail signature [WIP]
+function hideSig() {
+	        let x = document.getElementsByClassName('squireIframe');
+          x[0].style.display = 'none';
+	
+	let x = window.frames
+     	
+	 for (var i = 0; i <= x.length; i++) { 
+         x[i].document.body.getElementsByClassName('protonmail_signature_block-proton')[0].innerHTML = " ";
+	}
+} */
